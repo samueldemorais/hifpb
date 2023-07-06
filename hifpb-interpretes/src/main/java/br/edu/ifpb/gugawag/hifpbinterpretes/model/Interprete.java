04bc9c7fc8849f66f7467c0eb1441324736ed9af
samueldemorais <ssamueldemorais@gmail.com>
2023-07-06 13:41:57 -0300

    update

--- hifpb-interpretes/src/main/java/br/edu/ifpb/gugawag/hifpbinterpretes/model/Interprete.java
+++ hifpb-interpretes/src/main/java/br/edu/ifpb/gugawag/hifpbinterpretes/model/Interprete.java
@@ -1,5 +1,26 @@
 package br.edu.ifpb.gugawag.hifpbinterpretes.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import jakarta.persistence.CascadeType;
+
+@Entity
 public class Interprete {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public String id;
+
+    @Column()
+    public String nome;
+  
+    @Column()
+    public String cpf;
     
+	@ManyToMany(mappedBy="interpretes", cascade= {CascadeType.PERSIST, CascadeType.MERGE})
+    public ArrayList<Horario> horarios;
 }
